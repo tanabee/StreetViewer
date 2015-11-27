45820b87a89d2fd81b067a5270da9e2d2a17567e
yutanabe <yutanabe@yahoo-corp.jp>
2015-11-27 12:28:43 +0900

    ストリートビューを表示

---
+++ Assets/StreetViewImageLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreetViewImageLoader : MonoBehaviour {
+	
+	public double heading = 0.0;
+	public double pitch = 0.0;
+	
+	private int width = 640;
+	private int height = 480;
+	
+	private double longitude = 139.667431;
+	private double latitude = 35.697408;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetStreetViewImage(latitude, longitude, heading, pitch));
+	}
+	
+	private IEnumerator GetStreetViewImage(double latitude, double longitude, double heading, double pitch) {
+		string url = "http://maps.googleapis.com/maps/api/streetview?" + "size=" + width + "x" + height + "&location=" + latitude + "," + longitude + "&heading=" + heading + "&pitch=" + pitch + "&fov=90&sensor=false";
+		
+		WWW www = new WWW(url);
+		yield return www;
+		
+		GetComponent<Renderer>().material.mainTexture = www.texture;
+	}
+}
